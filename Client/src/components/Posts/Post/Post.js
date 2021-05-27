180d8d7c3a7784af03b5fc1bdbf53faf80c930ba
Salikcr <footballer.salik@gmail.com>
2021-05-27 20:33:33 +0530

    like functionality without authentication

--- Client/src/components/Posts/Post/Post.js
+++ Client/src/components/Posts/Post/Post.js
@@ -6,7 +6,7 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import {useDispatch} from 'react-redux';
-import {deletePost} from '../../../actions/posts';
+import {deletePost,likePost} from '../../../actions/posts';
 const Post = ({post,setCurrentId}) => {
     
     const classes = useStyles();
@@ -34,7 +34,7 @@ const Post = ({post,setCurrentId}) => {
                     <Typography  variant = "h5" gutterBottom >{post.message}</Typography>
                 </CardContent>
                 <CardActions className = {classes.cardActions}>
-                    <Button size = "small" color = "primary" onClick = {() => {}}>
+                    <Button size = "small" color = "primary" onClick = {() => dispatch(likePost(post._id))}>
                         <ThumbUpAltIcon fontSize = "small" />
                         Like
                         {post.likeCount}
--- Server/controllers/posts.js
+++ Server/controllers/posts.js
@@ -48,3 +48,14 @@ export const deletePost  = async (req,res) => {
     res.json({message : 'post deleted successfully'});
 
 }
+
+export const likePost = async(req,res) => {
+    const {id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)) return  res.status(404).send("No post found");
+
+    const post = await PostMessage.findById(id);
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id,{likeCount: post.likeCount + 1}, {new:true});
+
+    res.json(updatedPost);
+}

ab5cd225629c2d8a989fcf6c73299711e27b6853
Salikcr <footballer.salik@gmail.com>
2021-06-14 20:28:59 +0530

    added googleOAuth

--- Client/src/App.js
+++ Client/src/App.js
@@ -6,41 +6,30 @@ import {getPosts} from './actions/posts';
 import Posts from './components/Posts/Posts.js';
 import Form from './components/Form/Form.js';
 import useStyles from './styles.js';
-
+import {BrowserRouter,Switch,Route} from 'react-router-dom';
 
 
 import memories from './images/memories.png';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 
 const App = () => {
 
-    const [currentId,setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-
-
-    useEffect(() => {
-      dispatch(getPosts());
-    },[currentId,dispatch]);
+    
 
     return  (
+      <BrowserRouter>
         <Container maxWidth="lg">
-      <AppBar  className = {classes.appBar} position="static" color="inherit">
-        <Typography className = {classes.heading} variant="h2" align="center">Memories</Typography>
-        <img  className = {classes.image} src={memories} alt="icon" height="60"/>
-      </AppBar>
-        <Grow in>
-          <Container>
-            <Grid container justify = "space-between" alignItems = "stretch" spacing = {3}>
-              <Grid item xs = {12} sm = {7}>
-                <Posts setCurrentId = {setCurrentId}/>
-              </Grid>
-              <Grid item xs = {12} sm = {4}>
-                <Form currentId = {currentId} setCurrentId = {setCurrentId}/>
-              </Grid>
-            </Grid>
-          </Container>
-        </Grow>      
+        <Navbar/>
+        <Switch>
+          <Route path = "/" exact component = {Home}></Route>
+          <Route path = "/auth" exact component = {Auth}></Route>
+          
+        </Switch>
+         
         </Container>
+        </BrowserRouter>
     );
 }
 export default App;
---
+++ Client/src/components/Auth/Auth.js
@@ -0,0 +1,105 @@
+import React,{useState} from 'react'
+import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from '@material-ui/core';
+import useStyles from './styles';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Input from './Input';
+import {GoogleLogin} from 'react-google-login';
+import Icon from './Icon';
+import {useDispatch} from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+const Auth = () => {
+
+    const classes = useStyles();
+    // const isSignUp = true;
+    const history = useHistory();
+    const [showPassword,setShowPassword] = useState(false);
+    const [isSignUp,setIsSignup] = useState(false);
+    const dispatch = useDispatch();
+
+    const handleSubmit = () => {
+
+    };
+
+    const handleChange = () => {
+
+    };
+
+    const switchMode = () => {
+        setIsSignup((prevIsSignup) => !prevIsSignup);
+        handleShowPassword(false);
+    };
+
+    const googleSuccess = async (res) => {
+        const result = res?.profileObj; // ?  for not if not getting res 
+        const token = res?.tokenId;
+
+        try {
+            dispatch({type:"AUTH",data : {result,token}});
+            history.push('/');
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    const googleFailure = (error) => {
+        console.log(error);
+        console.log("Google sign in was un successfull");
+    }
+
+    const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword)
+
+    return (
+        <Container component = "main" maxWidth = "xs">
+            <Paper className  = {classes.paper} elevation = {3}>
+                <Avatar className = {classes.avatar}>
+                    <LockOutlinedIcon/>
+                </Avatar>
+                <Typography variant = "h5">
+                    {isSignUp ? 'Sign Up' : 'Sign In'}
+                </Typography>
+                <form className = {classes.form} onSubmit = {handleSubmit}>
+                    <Grid container spacing = {2}>
+                        {
+                            isSignUp && (
+                                <>
+                                    <Input name = "firstname" label = "First Name" handleChange = {handleChange} autoFocus half/>
+                                    <Input name="lastName" label="Last Name" handleChange={handleChange} half />                                
+                                </>
+                            )
+                        }
+                        <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+                        <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
+                        {isSignUp &&  <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />}
+                        <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                            { isSignUp ? 'Sign Up' : 'Sign In' }
+                        </Button>
+                        <GoogleLogin 
+                            clientId = "540575146666-pe6a7iamnd3igkm8u93a31sgd69m1jac.apps.googleusercontent.com"
+                            render = {(renderProps) => (
+                                <Button className = {classes.googleButton} color = "primary" fullWidth onClick = {renderProps.onClick} disabled = {renderProps.disabled} startIcon = {<Icon/>} variant = "contained">
+                                    Google Sign In
+                                </Button>
+                            )}
+                            onSuccess={googleSuccess}
+                            onFailure={googleFailure}
+                            cookiePolicy="single_host_origin"
+                        />
+                        <Grid container justify="flex-end">
+                            <Grid item>
+                                <Button onClick={switchMode}>
+                                    { isSignUp ? 'Already have an account? Sign in' : "Don't have an account? Sign Up" }
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+
+                </form>
+            </Paper>
+        </Container>
+    )
+}
+
+
+export default Auth;
